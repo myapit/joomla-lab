04b376d95539952b31b4d49c0fe171c26d067cbf
myapit <9w2xhr@gmail.com>
2016-11-11 12:06:09 +0800

    create joomla plugin to fix json issue on 3.6.x -- add new replace rules

--- plg_jsonfix/jsonfix.utf8.sql
+++ plg_jsonfix/jsonfix.utf8.sql
@@ -1,7 +1,20 @@
 UPDATE #__users SET params = REPLACE(params,'&quot;','"');
+
 UPDATE #__categories SET params = REPLACE(params,'&quot;','"');
 UPDATE #__categories SET metadata= REPLACE(metadata,'&quot;','"');
+
 UPDATE #__content SET images = REPLACE(images,'&quot;','"');
 UPDATE #__content SET urls = REPLACE(urls,'&quot;','"');
 UPDATE #__content SET attribs = REPLACE(attribs,'&quot;','"');
 UPDATE #__content SET metadata = REPLACE(metadata,'&quot;','"');
+
+
+UPDATE #__users SET params = REPLACE(params,'&amp;quot;','"');
+
+UPDATE #__categories SET params = REPLACE(params,'&amp;quot;','"');
+UPDATE #__categories SET metadata= REPLACE(metadata,'&amp;quot;','"');
+
+UPDATE #__content SET images = REPLACE(images,'&amp;quot;','"');
+UPDATE #__content SET urls = REPLACE(urls,'&amp;quot;','"');
+UPDATE #__content SET attribs = REPLACE(attribs,'&amp;quot;','"');
+UPDATE #__content SET metadata = REPLACE(metadata,'&amp;quot;','"');
